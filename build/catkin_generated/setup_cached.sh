b73d22c609704d93668c04dc74afdfa840e93f8e
Sam-Skupien <s.skupien1@gmail.com>
2022-11-09 03:33:45 +0000

    First commit.

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/workspace/catkin_ws/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.0/lib64:/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib/i386-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
+export PWD="/home/workspace/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/workspace/catkin_ws/src:/opt/ros/kinetic/share"
